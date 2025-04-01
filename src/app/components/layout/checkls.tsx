1812e3ab0a8e78b8f1e84bbcab3c6de308321014
eunice-ogboye <ogboyeeunice@gmail.com>
2025-04-01 23:53:24 +0100

    fix: LANDING PAGE

--- src/app/components/layout/checkls.tsx
+++ src/app/components/layout/checkls.tsx
@@ -1,24 +1,24 @@
-import { CheckCircleIcon } from "@heroicons/react/solid";
+// import { CheckCircleIcon } from "@heroicons/react/solid";
 
-const Checklist = () => {
-  const items = [
-    "Mouve gives you more brand visibility like you have seen before",
-    "Get access to our pool of large customer base.",
-    "Onboard your riders and earn 500,000NGN monthly without stress",
-    "Register as an agent and earn while you deliver goods to people",
-    "Mouve provides you with a technology platform to easily track your riders",
-  ];
+// const Checklist = () => {
+//   const items = [
+//     "Mouve gives you more brand visibility like you have seen before",
+//     "Get access to our pool of large customer base.",
+//     "Onboard your riders and earn 500,000NGN monthly without stress",
+//     "Register as an agent and earn while you deliver goods to people",
+//     "Mouve provides you with a technology platform to easily track your riders",
+//   ];
 
-  return (
-    <div className="space-y-4 bg-gray-900 p-6 rounded-lg max-w-lg mx-auto">
-      {items.map((item, index) => (
-        <div key={index} className="flex items-start space-x-3">
-          <CheckCircleIcon className="w-6 h-6 text-green-500" />
-          <p className="text-gray-300 text-lg">{item}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div className="space-y-4 bg-gray-900 p-6 rounded-lg max-w-lg mx-auto">
+//       {items.map((item, index) => (
+//         <div key={index} className="flex items-start space-x-3">
+//           <CheckCircleIcon className="w-6 h-6 text-green-500" />
+//           <p className="text-gray-300 text-lg">{item}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-export default Checklist;
+// export default Checklist;
